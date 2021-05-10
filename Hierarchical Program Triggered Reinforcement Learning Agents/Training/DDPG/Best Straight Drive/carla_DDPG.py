f968af2cad509ac9ce57fa844db9522213932902
Briti Gangopadhyay <30759817+britig@users.noreply.github.com>
2021-05-10 16:20:30 +0530

    Update carla_DDPG.py

--- Hierarchical Program Triggered Reinforcement Learning Agents/Training/DDPG/Best Straight Drive/carla_DDPG.py
+++ Hierarchical Program Triggered Reinforcement Learning Agents/Training/DDPG/Best Straight Drive/carla_DDPG.py
@@ -18,6 +18,7 @@ ROUNDING_FACTOR = 3
 
 try:
 	sys.path.append(glob.glob('../../../carla/dist/carla-*%d.%d-%s.egg' % (
+		sys.version_info.major,
 		sys.version_info.minor,
 		'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
 except IndexError:
